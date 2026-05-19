bfa664236477cee3c0686166e43acda1ae0c69c3
aniss <anisse2000@icloud.com>
2026-05-19 15:06:46 +0200

    cessfin2

--- mes_evenements.php
+++ mes_evenements.php
@@ -71,5 +71,9 @@
     }
 ?>
 </div>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="Script/index.js"></script>
+<script  src="jquery.js"></script>
+    <script  src="menu.js"></script>
 </body>
 </html>
